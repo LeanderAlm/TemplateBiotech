74f56928798e8c036ec0a769238522706b9dd71f
Leander Santos <116657748+LeanderAlm@users.noreply.github.com>
2023-05-08 23:12:14 -0300

    Add files via upload

---
+++ Pagina inicial/js.js
@@ -0,0 +1,7 @@
+
+var dataAtual = new Date();
+var dia = dataAtual.getDate();
+var mes = dataAtual.getMonth() + 1;
+var ano = dataAtual.getFullYear();
+var dataFormatada = dia + "/" + mes + "/" + ano;
+document.querySelector('.data').innerHTML = dataFormatada;
